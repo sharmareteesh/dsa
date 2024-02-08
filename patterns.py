614f63df6ff4bfc405a1115e32adceb7902f5c77
sharmareteesh <sarmareteesh@gmail.com>
2024-02-08 14:37:51 +0530

    Test

---
+++ patterns.py
@@ -0,0 +1,48 @@
+
+n = int(input("Enter the length of patterns : "))
+
+
+print("1'st Question")
+
+for i in range(n):
+    for j in range(n):
+        print("* ",end="")
+    print()
+
+
+print("2'th Question")
+
+for i in range(n):
+    for j in range(i+1):
+        print("* ",end="")
+    print()
+
+print("3'th Question")
+
+for i in range(n):
+    for j in range(i+1):
+        print(j + 1 ,end="")
+    print()
+
+print("4'th Question")
+
+for i in range(n):
+    for j in range(i+1):
+        print(i+1,end="")
+    print()
+
+print("5'th Question")
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("* ",end="")
+    print()
+
+print("6'n Question")
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print(j+1,end="")
+    print()
+
+print("7'th question ")
