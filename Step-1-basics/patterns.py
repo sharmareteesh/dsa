9c898b6c1ad3d1e1ddf7e69a5816ebd57395ea7c
sharmareteesh <sharmareteesh@gmail.com>
2025-03-27 21:11:58 +0530

    Step-1-basics

--- Step-1-basics/patterns.py
+++ Step-1-basics/patterns.py
@@ -63,4 +63,35 @@ def patterns(value):
         for j in range(i):
             print((j%2)," ",end="")
         print()
+
+    print("Question :12")
+    for i in range(1,value+1):
+        for j in range(1,i+1):
+            print(j,end="")
+        print(" "*(2*(value-i)),end="")
+        for j in range(i,0,-1):
+            print(j,end="")
+        print()
+
+    print("Question : 13")
+    holder=1
+    for i in range(1,value+1):
+        for j in range(i):
+            print(holder,end="")
+            holder+=1
+        print()
+
+    print("Question :14")
+    for i in range(1,value+1):
+        for j in range(i):
+            print(chr(65+j),end="")
+        print()
+
+    print("Question :14")
+    for i in range(value+1,0,-1):
+        for j in range(i):
+            print(chr(65+j),end="")
+        print()
+
+
 patterns(n)
